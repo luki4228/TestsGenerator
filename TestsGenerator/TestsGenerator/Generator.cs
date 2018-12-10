8c7f82dec981ccbca05615d60895feee05d0acba
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-12-10 23:40:23 +0300

    added data structures and generate method

---
+++ TestsGenerator/TestsGenerator/Generator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace TestsGenerator
+{
+    public class Generator
+    {
+        public async Task Generate(List<string> inputFiles, string outputPath)
+        {
+            
+        }
+    }
+}
---
+++ TestsGenerator/TestsGenerator/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class Test
+    {
+        public Test(string name, string content)
+        {
+            Name = name;
+            Content = content;
+        }
+
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
