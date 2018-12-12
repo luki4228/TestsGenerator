0cdd71d84dcb1a593ae68979cf81004439c61e31
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-12-12 11:27:13 +0300

    added Unit Tests

---
+++ TestsGenerator/GeneratorTests/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorTests
+{
+    public class Class1
+    {
+        public Class1(string _nothing)
+        {
+            Nothing = _nothing;
+        }
+        public string Nothing { get; set; }
+
+        public void Method1()
+        {
+            string temp = "asdasdasd";
+            temp = temp + "qwerty";
+            Nothing = Nothing + temp;
+        }
+    }
+
+    public class Class2
+    {
+        public Class2(string _nothing)
+        {
+            Nothing = _nothing;
+        }
+        public string Nothing { get; set; }
+
+        public string Method2()
+        {
+            string temp = "asdasdasd";
+            Nothing = temp;
+            return Nothing;
+        }
+    }
+}
--- TestsGenerator/GeneratorTests/UnitTest1.cs
+++ TestsGenerator/GeneratorTests/UnitTest1.cs
@@ -1,14 +1,84 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+using TestsGenerator;
 
 namespace GeneratorTests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private List<CInfo> parcedClasses;
+        private List<Test> generatedTests;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            string path = "Class1.cs";
+
+            int maxReading = 2;
+            int maxWriting = 3;
+            int maxProcessing = 4;
+
+            var generator = new Generator(maxReading, maxWriting, maxProcessing);
+
+            string sourceCode;
+            using (StreamReader strmReader = new StreamReader(path))
+            {
+                sourceCode = strmReader.ReadToEnd();
+            }
+
+            var parcer = new CodeParcer();
+            parcedClasses = parcer.Parce(sourceCode);
+            generatedTests = generator.GenerateTests(sourceCode);
+        }
+
+        [TestMethod]
+        public void ClassCountTest()
+        {
+            Assert.AreEqual(parcedClasses.Count, 2);
+        }
+
+        [TestMethod]
+        public void ClassEqualNamespacesTest()
+        {
+            Assert.AreEqual(parcedClasses[0].Namespace, parcedClasses[1].Namespace);
+        }
+
+        [TestMethod]
+        public void ClassInfoTest()
+        {
+            Assert.AreEqual(parcedClasses[0].Namespace, "UnitTests");
+            Assert.AreEqual(parcedClasses[1].Namespace, "UnitTests");
+            Assert.AreEqual(parcedClasses[0].Name, "Class1");
+            Assert.AreEqual(parcedClasses[1].Name, "Class2");
+            Assert.AreEqual(parcedClasses[0].Methods[0], "Method1");
+            Assert.AreEqual(parcedClasses[1].Methods[0], "Method2");
+        }
+
+        [TestMethod]
+        public void TestClassesAmountTest()
+        {
+            Assert.AreEqual(generatedTests.Count, 2);
+        }
+
+        [TestMethod]
+        public void TestNameTest()
+        {
+            Assert.AreEqual(generatedTests[0].Name, "Class1Test.cs");
+            Assert.AreEqual(generatedTests[1].Name, "Class2Test.cs");
+        }
+
+        [TestMethod]
+        public void TestContentTest()
+        {
+            generatedTests[0].Content.Contains("GeneratorTests.Tests");
+            generatedTests[1].Content.Contains("GeneratorTests.Tests");
+            generatedTests[0].Content.Contains("Class1Test");
+            generatedTests[1].Content.Contains("Class2Test");
+            generatedTests[0].Content.Contains("Method1Test");
+            generatedTests[1].Content.Contains("Method2Test");
         }
     }
 }
